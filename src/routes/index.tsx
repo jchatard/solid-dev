b96cf9a7aa0e316bd08d1da3a303f536ea9cd0be
Jérémy Chatard <jchatard@breek.fr>
2023-04-12 11:16:48 +0200

    Repro.

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,11 +1,17 @@
 import { Title } from "solid-start";
 import Counter from "~/components/Counter";
+import {DEV, Show} from "solid-js";
 
 export default function Home() {
   return (
     <main>
       <Title>Hello World</Title>
       <h1>Hello world!</h1>
+
+      <Show when={DEV} fallback={<h2>I'm in PROD mode</h2>}>
+        <h2>I'm in DEV mode</h2>
+      </Show>
+
       <Counter />
       <p>
         Visit{" "}
